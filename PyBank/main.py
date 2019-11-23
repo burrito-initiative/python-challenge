452c891905a14b84c33ab621da5201f6e43093ea
Jon Disarufino <jondisarufino@outlook.com>
2019-11-23 16:31:15 -0600

    uploading hw files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,48 @@
-# new
+## BANK FINANCIAL ANALYSIS
+# Script Dependencies
+import os
+import pandas as pd
+
+# Open CSV file resource
+csvfile = "Resources/budget_data.csv"
+budget_df = pd.read_csv(csvfile)
+budget_index_df = budget_df.set_index("Profit/Losses")
+# print(budget_df.head()) #test readability
+
+# Formulas
+months_count = budget_df["Date"].count()
+total_profit = budget_df["Profit/Losses"].sum()
+profit_diff_avg = round((budget_df["Profit/Losses"]-budget_df["Profit/Losses"].shift(1)).mean(), 2)
+max_profit = budget_df["Profit/Losses"].max()
+max_month = budget_index_df.loc[max_profit,"Date"]
+min_profit = budget_df["Profit/Losses"].min()
+min_month = budget_index_df.loc[min_profit,"Date"]
+
+# Print Preview
+print("-----------------------------------")
+print("~ FINANCIAL ANALYSIS PREVIEW ~")
+print("-----------------------------------")
+print(f"Total Months: {months_count}")
+print(f"Total: ${total_profit}")
+print(f"Average monthly Profit/Loss: ${profit_diff_avg}")
+print(f"Largest Profit: {max_month}, ${max_profit}")
+print(f"Largest Loss: {min_month}, ${min_profit}")
+print("-----------------------------------")
+print(f"* See new text report 'budget_report.txt'")
+print(" ")
+
+# Create file and destination:
+output_path = os.path.join("budget_report.txt")
+
+# Write Report
+with open(output_path, 'w', newline='') as text_file:
+    text_file.write("FINANCIAL ANALYSIS:" + "\n")
+    text_file.write("-----------------------------------" + "\n")
+    text_file.write(f"Total Months: {months_count}" + "\n")
+    text_file.write(f"Total: ${total_profit}" + "\n")
+    text_file.write(f"Average monthly Profit/Loss: ${profit_diff_avg}" + "\n")
+    text_file.write(f"Largest Profit: {max_month}, ${max_profit}" + "\n")
+    text_file.write(f"Largest Loss: {min_month}, ${min_profit}")
+
+# Close
+text_file.close
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,67 @@
-# new
+### ELECTION ANALYSIS
+# Script Dependencies
+import os
+import pandas as pd
+
+# >>> Open CSV file resource and define dataframe
+csvfile = "Resources/election_data.csv"
+election_df = pd.read_csv(csvfile)
+    # # ------ dataframe preview ------ 
+    # print(election_df.head()) 
+    # # headers preview
+    # print(election_df.columns) = ['Voter ID', 'County', 'Candidate']
+
+# >>> Find total number of votes 
+all_votes = election_df["Candidate"].count()
+    # # ------ total preview ------ 
+    # print(all_votes)
+    # print(all_votes.dtype) # confirmed that value is int32
+
+# >>> Summary: Group by Candidates
+candidate_summary_df = election_df.groupby(["Candidate"])
+# Count number of votes by Candidate group
+candidate_summary_df = candidate_summary_df.count()
+# Total Votes - Take the totals counted above and assign to name "Total Votes"
+candidate_summary_df["Total Votes"] = candidate_summary_df["Voter ID"]
+# Percent Votes - Add a percent column and format to rounded %
+candidate_summary_df["Percent Votes"] = ((candidate_summary_df["Total Votes"] / all_votes)*100).map("{:,.1f}%".format)
+# Finalize new data frame and reset index from the groupby
+final_summary_df = (candidate_summary_df[["Total Votes","Percent Votes"]]).reset_index()
+    # # ------ Preview Final Summary ------
+    # print(final_summary_df)
+
+# >>> Find the winner
+most_votes = final_summary_df["Total Votes"].max()
+    # # ------ preview most votes for a candidate ------
+    # print(most_votes)
+winner_name = final_summary_df.loc[final_summary_df["Total Votes"] == most_votes, "Candidate"].values[0]
+    # # ------ preview winner name ------
+    # print(winner_name)
+
+# >>> PRINT PREVIEW
+print("-----------------------------------")
+print("~ ELECTION RESULTS PREVIEW ~")
+print("-----------------------------------")
+print(f"All Votes Cast: {all_votes}")
+print("Results:")
+print(final_summary_df)
+print("-----------------------------------")
+print(f"Popular vote winner: {winner_name} with {most_votes} votes!")
+print("-----------------------------------")
+print(f"* See new text report 'election_report.txt'")
+print("")
+
+# >>> Create file and destination:
+output_path = os.path.join("election_report.txt")
+
+# Write Report
+with open(output_path, 'w', newline='') as text_file:
+    text_file.write("ELECTION RESULTS:" + "\n")
+    text_file.write("-----------------------------------" + "\n")
+    text_file.write(f"All Votes Cast: {all_votes}" + "\n")
+    text_file.write(f"{final_summary_df}" + "\n")
+    text_file.write("-----------------------------------" + "\n")
+    text_file.write(f"Popular vote winner: {winner_name} with {most_votes} votes!")
+    
+# Close
+text_file.close
